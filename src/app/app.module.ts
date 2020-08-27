3fc557cc8525dbaa698123f31634b2831c65a031
AudreyChob <62380768+AudreyChob@users.noreply.github.com>
2020-08-27 17:43:38 +0200

    [DEV] app todolist

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+export const firebaseConfig = {
+  apiKey: "AIzaSyBf7NCpAa8bXiZOAIni3nLNadngxxIED1s",
+  authDomain: "todolist-bbcb5b.firebaseapp.com",
+  databaseURL: "https://todolist-bbcb5b.firebaseio.com",
+  projectId: "todolist-bbcb5b",
+  storageBucket: "todolist-bbcb5b.appspot.com",
+  messagingSenderId: "486632956520",
+  appId: "1:486632956520:web:246e6856def6b2533d51dd"
+};
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
